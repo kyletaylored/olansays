11a593151d74be2980c8e9bf116639e044a2411e
Kyle Taylor <kyletaylored@gmail.com>
2015-06-18 14:14:54 -0500

    image/code update

--- js/olansays.js
+++ js/olansays.js
@@ -2,12 +2,27 @@ jQuery(document).ready(function($) {
 	init();
 });
 
-var spreadsheetID = "1Nbn2-8niXdCwGj97raH4PHSv4MJqUhNMu4799G9Qy8s";
-
 function init() {
-	Tabletop.init( { key: spreadsheetID,
+
+	var spreadsheetID = "1Nbn2-8niXdCwGj97raH4PHSv4MJqUhNMu4799G9Qy8s";
+	var sayings = "sayings";
+
+	// Add values to localStorage for future reference, updating incrementally.
+	if(typeof(Storage)!=="undefined") {
+		console.log("Yay, you have web storage! I think I left my watch in here somewhere...");
+		if (!localStorage.sayings || sayings.length > localStorage.sayings.length) {
+			localStorage.sayings = sayings;
+		}
+	} else {
+		console.log("Sorry, no web storage for you. :(");
+	}
+
+	Tabletop.init({
+		key: spreadsheetID,
 		callback: showInfo,
-		simpleSheet: true } );
+		simpleSheet: true
+	});
+
 }
 
 function showInfo(data, tabletop) {
@@ -16,10 +31,11 @@ function showInfo(data, tabletop) {
 	var numRes = data.length;
 	var rand = Math.floor(Math.random() * numRes);
 	str = data[rand].olanSays;
+	// Replace new lines with breaks.
 	str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
 
 	$(data).each(function(){
     // Column names are name, age, etc.
-		$('#olanSays').html(str);
+		$('#olanSays').animate().html(str);
   });
 }
