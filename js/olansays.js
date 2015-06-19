b812dc9d6734d25d9ff11af446214e6e655364b7
Kyle Taylor <kyletaylored@gmail.com>
2015-06-19 02:57:41 -0500

    updates

--- js/olansays.js
+++ js/olansays.js
@@ -1,41 +1,62 @@
-jQuery(document).ready(function($) {
-	init();
-});
+// OlanSays.js
 
-function init() {
-
+function getSayings() {
 	var spreadsheetID = "1Nbn2-8niXdCwGj97raH4PHSv4MJqUhNMu4799G9Qy8s";
-	var sayings = "sayings";
-
-	// Add values to localStorage for future reference, updating incrementally.
-	if(typeof(Storage)!=="undefined") {
-		console.log("Yay, you have web storage! I think I left my watch in here somewhere...");
-		if (!localStorage.sayings || sayings.length > localStorage.sayings.length) {
-			localStorage.sayings = sayings;
-		}
-	} else {
-		console.log("Sorry, no web storage for you. :(");
-	}
 
 	Tabletop.init({
 		key: spreadsheetID,
-		callback: showInfo,
-		simpleSheet: true
+		simpleSheet: true,
+		callback: function(data){
+			// Add values to localStorage for future reference, updating incrementally.
+			var olanStorage = localStorage.getItem("olanSayings");
+			olanStorage = JSON.parse(olanStorage);
+
+			if (!olanStorage || data.length > olanStorage.length) {
+				localStorage.setItem("olanSayings", JSON.stringify(data));
+			}
+		}
 	});
-
 }
 
-function showInfo(data, tabletop) {
-	console.log(data);
+function swapSaying(olanStorage) {
 
-	var numRes = data.length;
-	var rand = Math.floor(Math.random() * numRes);
-	str = data[rand].olanSays;
-	// Replace new lines with breaks.
-	str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
+	if (!olanStorage || olanStorage === null) {
+		olanStorage = localStorage.getItem("olanSayings");
+		olanStorage = JSON.parse(olanStorage);
+	}
 
-	$(data).each(function(){
-    // Column names are name, age, etc.
-		$('#olanSays').animate().html(str);
-  });
+	if (olanStorage === null) {
+		// Replace saying.
+		$('#olanSays').html("It's a Monday.");
+	} else {
+		var numRes = olanStorage.length;
+		var rand = Math.floor(Math.random() * numRes);
+		var str = olanStorage[rand].olanSays;
+
+		// Replace new lines with breaks.
+		str = str.replace(/(?:\r\n|\r|\n)/g, '<br />');
+
+		// Replace saying.
+		$('#olanSays').html(str);
+	}
 }
+
+// Main runtime.
+jQuery(document).ready(function($) {
+
+	if(typeof(Storage)!=="undefined") {
+		window.console.log("Yay, you have web storage! I think I left my watch in here somewhere...");
+		
+		var olanStorage = localStorage.getItem("olanSayings");
+		olanStorage = JSON.parse(olanStorage);
+
+		if (olanStorage && olanStorage.length > 0) {
+			swapSaying(olanStorage);
+			getSayings();
+		} else {
+			getSayings();
+			swapSaying();
+		}
+	}
+	
+});
